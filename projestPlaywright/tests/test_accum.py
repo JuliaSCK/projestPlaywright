78f6a8a80ffa155147dff417cef88a12cb3cefeb
Yuliys <118895869+JuliaSCK@users.noreply.github.com>
2024-03-14 12:16:27 +0200

    Add files via upload

---
+++ projestPlaywright/tests/test_accum.py
@@ -0,0 +1,43 @@
+from source.accum import Accumulator
+import pytest
+
+
+@pytest.fixture
+def accum2():
+    return Accumulator()
+
+
+def test_new_accum(accum, accum2):
+    assert accum.count == 0
+    assert accum2.count == 0
+
+
+def test_add_counts(accum, accum_global):
+    accum.add_counts()
+    assert accum.count == 1
+    assert accum_global.count == 0
+
+
+@pytest.mark.sanity
+def test_add_counts_twice(accum):
+    accum.add_counts()
+    accum.add_counts()
+    assert accum.count == 2
+
+@pytest.mark.sanity
+def test_add_counts_with_params(accum):
+    accum.add_counts(10)
+    assert accum.count == 10
+
+@pytest.mark.regression
+def test_add_counts_and_setter(accum):
+    accum.count = 2
+    accum.add_counts()
+    assert accum.count == 3
+
+
+def test_get_brand(accum):
+    with pytest.raises(AttributeError) as err:
+        print(accum.__brand)
+
+    assert str(err.value) == "'Accumulator' object has no attribute '__brand'"
