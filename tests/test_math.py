ca791869656bad229a1494d492007a799740c7cf
JuliaSCK <118895869+JuliaSCK@users.noreply.github.com>
2024-03-14 12:18:55 +0200

    first push

---
+++ tests/test_math.py
@@ -0,0 +1,45 @@
+import pytest
+import math
+
+
+@pytest.mark.sanity
+def test_one_add_one():
+    assert 1 + 1 == 2
+
+
+@pytest.mark.regression
+def test_math_pow():
+    assert math.pow(3, 3) == 27
+
+
+@pytest.mark.sanity
+def test_one_add_two():
+    num_1 = 1
+    num_2 = 2
+    result = 4
+    print(f"{result = }")
+    assert num_1 + num_2 != result, 'A result should be 3'
+
+
+@pytest.mark.regression
+def test_divide_by_zero():
+    with pytest.raises(ZeroDivisionError) as err:
+        num_1 = 5 / 0
+
+    assert str(err.value) == 'division by zero'
+    assert err.type == ZeroDivisionError
+
+
+#  Parametrized Test -> DRY - Dry Repeat Yourself
+test_data = [
+    (2, 3, 6),
+    (99, 1, 99),
+    (0, 10, 0),
+    (-5, 1, -5),
+    (-5, -2, 10)
+]
+
+
+@pytest.mark.parametrize("num_1, num_2, result", test_data)
+def test_multiplication(num_1, num_2, result):
+    assert num_1 * num_2 == result
